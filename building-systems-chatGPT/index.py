05175e2c732729c96b9c92dbd10ec1f657f5584f
vn <victor@Victors-MacBook-Pro.local>
2023-07-30 11:30:05 -0700

    Add example about classification

--- building-systems-chatGPT/index.py
+++ building-systems-chatGPT/index.py
@@ -14,3 +14,53 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
         temperature=0,
     )
     return response.choices[0].message["content"]
+
+# Classification
+def classification_example():
+    delimiter = "####"
+    system_message = f"""
+    You will be provided with customer service queries. \
+    The customer service query will be delimited with \
+    {delimiter} characters.
+    Classify each query into a primary category \
+    and a secondary category. 
+    Provide your output in json format with the \
+    keys: primary and secondary.
+
+    Primary categories: Billing, Technical Support, \
+    Account Management, or General Inquiry.
+
+    Billing secondary categories:
+    Unsubscribe or upgrade
+    Add a payment method
+    Explanation for charge
+    Dispute a charge
+
+    Technical Support secondary categories:
+    General troubleshooting
+    Device compatibility
+    Software updates
+
+    Account Management secondary categories:
+    Password reset
+    Update personal information
+    Close account
+    Account security
+
+    General Inquiry secondary categories:
+    Product information
+    Pricing
+    Feedback
+    Speak to a human
+
+    """
+    user_message = f"""\
+    I want you to delete my profile and all of my user data"""
+    messages =  [  
+    {'role':'system', 
+    'content': system_message},    
+    {'role':'user', 
+    'content': f"{delimiter}{user_message}{delimiter}"},  
+    ] 
+    response = get_completion_from_messages(messages)
+    print(response)
